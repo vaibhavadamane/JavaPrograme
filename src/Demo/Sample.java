c1e079510c3f18b121169fb806b9fb6831fd20bb
vaibhavadamane <vaibhavadamane@2011gmil.com>
2022-11-22 20:20:09 +0530

    first commit

---
+++ src/Demo/Sample.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("good morning"); 
+
+	}
+
+}
---
+++ src/Demo/sample2.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class sample2 {
+
+	public static void main(String[] args) 
+	{
+System.out.println("hi my name is DON");
+
+
+	}
+
+}
